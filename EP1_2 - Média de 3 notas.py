2b6889a693aeb64ef09058f450acaf6ee8829326
lucianorvk7 <luciano.costantino@gmail.com>
2021-07-11 23:55:51 -0300

    Média de 3 notas

---
+++ EP1_2 - Média de 3 notas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 18:23:31 2021
+
+@author: Luciano Constantino RA:11201921468
+"""
+n1=float(input())
+n2=float(input())
+n3=float(input())
+media=(n1 + n2 + n3)/3
+print("%.2f" %(media))
+
